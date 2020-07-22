a6959d1d16aa7431a77e86dd1b9ef7215dacbf0b
Barri <info@bdesignstudio.co.uk>
2020-07-22 17:12:21 +0100

    Add faker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import faker from 'faker'
 
 const App =() =>{
 
@@ -7,7 +8,37 @@ const App =() =>{
   <div className="ui container comments">
     <div className="comment">
       <a href="/" className="avatar">
-        <img src="#" alt="avatar"/>
+        <img src={faker.image.avatar()} alt={"avatar"}/>
+      </a>
+      <div className="content">
+        <a href="" className="author">
+          Zarthan
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 5:00 PM</span>
+        </div>
+        <div className="text">Nice blog post!</div>
+      </div>
+    </div>
+    
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img src={faker.image.avatar()} alt={"avatar"}/>
+      </a>
+      <div className="content">
+        <a href="" className="author">
+          Zarthan
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 5:00 PM</span>
+        </div>
+        <div className="text">Nice blog post!</div>
+      </div>
+    </div>
+    
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img src={faker.image.avatar()} alt={"avatar"}/>
       </a>
       <div className="content">
         <a href="" className="author">
