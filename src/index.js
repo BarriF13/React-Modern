a1a2c01fd2a9c60aadf4df58f8980cefab26f160
Barri <info@bdesignstudio.co.uk>
2020-07-22 16:55:46 +0100

    Add some jsx

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App =() =>{
-  return <div>
-    Hello
-  </div>
-}
+
+  return (
+  <div className="ui container comments">
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img src="#" alt="avatar"/>
+      </a>
+      <div className="content">
+        <a href="" className="author">
+          Zarthan
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 5:00 PM</span>
+        </div>
+        <div className="text">Nice blog post!</div>
+      </div>
+    </div>
+  </div> 
+  );
+};
 ReactDOM.render(<App />, document.querySelector('#root'))
