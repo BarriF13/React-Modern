20b2cb979a90bb48f4c83d173223a18f644e28e9
Barri <info@bdesignstudio.co.uk>
2020-07-22 18:23:41 +0100

    Pass more props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import faker from 'faker';
+
 
 export const CommentDetail = (props) => {
-  console.log(props);
+  //console.log(props);
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img src={faker.image.avatar()} alt={"avatar"}/>
+        <img src={props.avatarImg} alt={"avatar"} />
       </a>
       <div className="content">
         <a href="" className="author">
-            {props.author}
+          {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 5:00 PM</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+       <div className="text">{props.commentText}</div>
       </div>
     </div>
   )
