2c221ac0e2ed5682ae83a99b8e616b054b23f77f
Barri <info@bdesignstudio.co.uk>
2020-07-22 18:44:09 +0100

    Make approval card comp

--- src/index.js
+++ src/index.js
@@ -2,13 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import faker from 'faker'
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 const App =() =>{
 
   return (
   <div className="ui container comments">
-   <CommentDetail author="Barri" timeAgo="Today at 5:00 pm" commentText ='please write more blogs' avatarImg = {faker.image.avatar()}/>
-   <CommentDetail author="Alice" timeAgo="Today at 4:00 pm" commentText ='Clever ideas' avatarImg = {faker.image.avatar()}/>
-   <CommentDetail author="Zarthani" timeAgo="Today at 12:00 pm" commentText ='Fantastic blog' avatarImg = {faker.image.avatar()}/>
+    <ApprovalCard/>
+    
+   <CommentDetail 
+   author="Barri"
+    timeAgo="Today at 5:00 pm" 
+    commentText ='please write more blogs' 
+    avatarImg = {faker.image.avatar()}
+    />
+
+   <CommentDetail 
+   author="Alice" 
+   timeAgo="Today at 4:00 pm" 
+   commentText ='Clever ideas' 
+   avatarImg = {faker.image.avatar()}
+   />
+   <CommentDetail
+    author="Zarthani" 
+    timeAgo="Today at 12:00 pm"
+    commentText ='Fantastic blog' 
+    avatarImg = {faker.image.avatar()}/>
     </div>
     
     
