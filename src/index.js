64d6cfadd504b82568b087c04dea1fcc1472c2cc
Barri <info@bdesignstudio.co.uk>
2020-07-22 17:27:23 +0100

    Nest component with hard coded data

--- src/index.js
+++ src/index.js
@@ -1,56 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import faker from 'faker'
-
+// import faker from 'faker'
+import CommentDetail from './CommentDetail'
 const App =() =>{
 
   return (
   <div className="ui container comments">
-    <div className="comment">
-      <a href="/" className="avatar">
-        <img src={faker.image.avatar()} alt={"avatar"}/>
-      </a>
-      <div className="content">
-        <a href="" className="author">
-          Zarthan
-        </a>
-        <div className="metadata">
-          <span className="date">Today at 5:00 PM</span>
-        </div>
-        <div className="text">Nice blog post!</div>
-      </div>
+   <CommentDetail/>
+   <CommentDetail/>
+   <CommentDetail/>
     </div>
     
-    <div className="comment">
-      <a href="/" className="avatar">
-        <img src={faker.image.avatar()} alt={"avatar"}/>
-      </a>
-      <div className="content">
-        <a href="" className="author">
-          Zarthan
-        </a>
-        <div className="metadata">
-          <span className="date">Today at 5:00 PM</span>
-        </div>
-        <div className="text">Nice blog post!</div>
-      </div>
-    </div>
     
-    <div className="comment">
-      <a href="/" className="avatar">
-        <img src={faker.image.avatar()} alt={"avatar"}/>
-      </a>
-      <div className="content">
-        <a href="" className="author">
-          Zarthan
-        </a>
-        <div className="metadata">
-          <span className="date">Today at 5:00 PM</span>
-        </div>
-        <div className="text">Nice blog post!</div>
-      </div>
-    </div>
-  </div> 
+
   );
 };
 ReactDOM.render(<App />, document.querySelector('#root'))
